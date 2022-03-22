396ba08f98833c757878f5ebdb6c6799753b2cb6
arifibnenam <arifibnenam@gmail.com>
2022-03-23 00:28:12 +0600

    cart calculation set

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = ({ cart }) => {
+    const priceCalc = (priceType) => {
+        let price = 0;
+        for (const item of cart) {
+            price += item[priceType];
+        }
+        return price;
+    }
+    const totalPrice = priceCalc('price');
+    const shippingCharge = priceCalc('shipping');
+    const tax = ((totalPrice + shippingCharge) * 5) / 100;
+    const grandTotal = totalPrice + shippingCharge + tax;
     return (
-        <div>
-            <h3>Order Summary</h3>
+        <div className='cart'>
+            <h3 className='cart-heading'>Order Summary</h3>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping Charge: ${shippingCharge}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal}</h4>
         </div>
     );
 };
